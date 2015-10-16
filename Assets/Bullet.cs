810d4d9ca035972c646384ceefd503f66e6bca0c
Low WeiLin <lwl1991@gmail.com>
2015-10-16 14:59:11 +0800

    Make bullets account for team

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 
 	private int damage = 1;
+	private int team = -2;
 
 	void OnCollisionEnter2D(Collision2D collision) {
 
-		// If what is hit has health, damage it.
-		if (collision.gameObject.GetComponent<Health> () != null) {
+		// If has health and not on same team, apply damage.
+		Health health = collision.gameObject.GetComponent<Health> ();
+		Player player = collision.gameObject.GetComponent<Player> ();
+		if ( health != null && (player == null || player.team != team) ) {
 			collision.gameObject.GetComponent<Health> ().TakeDamage (damage);
 			Destroy (gameObject);
 		}
@@ -16,6 +19,18 @@ public class Bullet : MonoBehaviour {
 
 	void Start() {
 		StartCoroutine(Timer());
+	}
+
+	public int getTeam() {
+		return team;
+	}
+
+	public void setTeam(int _team) {
+		if (team == -2) {
+			team = _team;
+		} else {
+			Debug.Log("Unable to set team for bullet!");
+		}
 	}
 
 	IEnumerator Timer() {
---
+++ Assets/ShooterBase.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShooterBase : MonoBehaviour {
+	
+	[SerializeField]
+	private GameObject prefab_bullet;
+
+	private Player player;
+	private int team = -1;
+
+	protected void Start() {
+		player = gameObject.GetComponent<Player> ();
+		if (player != null)
+			team = player.team;
+	}
+
+	public GameObject FireBullet(float directionDeg, float speed)
+	{
+		return FireBullet(directionDeg, speed, Vector3.zero);
+	}
+	
+	public GameObject FireBullet(float direction, float speed, Vector3 offset)
+	{
+		direction *= Mathf.PI/180f;
+		var velocity = new Vector3(Mathf.Cos(direction), Mathf.Sin(direction), 0)*speed;
+		return FireBullet(velocity, offset);
+	}
+	
+	public GameObject FireBullet(Vector3 velocity)
+	{
+		return FireBullet(velocity, Vector3.zero);
+	}
+	
+	public GameObject FireBullet(Vector3 velocity, Vector3 offset)
+	{
+		var bullet = Instantiate(prefab_bullet, transform.position + offset, prefab_bullet.transform.rotation) as GameObject;
+		bullet.GetComponent<Rigidbody2D> ().velocity = velocity;
+		bullet.GetComponent<Bullet> ().setTeam (team);
+		return bullet;
+	}
+}
