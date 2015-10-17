da1722f4e7417c6ea3cc179706cc353fdec87661
Low WeiLin <lwl1991@gmail.com>
2015-10-17 11:33:05 +0800

    Add human controller for manual play

---
+++ Assets/HumanController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanController : MonoBehaviour {
+
+	
+	protected Player player;
+	protected Movement2D movement2D;
+	protected float speed;
+	
+	protected void Start() {
+		player = gameObject.GetComponent<Player> ();
+		movement2D = gameObject.GetComponent<Movement2D> ();
+		speed = movement2D.maxSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 screenPoint = Input.mousePosition;
+		screenPoint.z = 10.0f;
+		movement2D.MoveTo (Camera.main.ScreenToWorldPoint(screenPoint));
+	}
+}
