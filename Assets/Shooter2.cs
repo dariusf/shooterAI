8abd3e85b116ae93a916e0ce008f82f36b9f1416
Low WeiLin <lwl1991@gmail.com>
2015-10-17 16:21:14 +0800

    Add randomness to shooter, versus mode

--- Assets/Shooter2.cs
+++ Assets/Shooter2.cs
@@ -6,16 +6,20 @@ public class Shooter2 : ShooterBase {
 	// Use this for initialization
 	new void Start () {
 		base.Start ();
-		cooldown = 0.3f;
-		Debug.Log (aiPlayer);
+		cooldown = 0.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (aiPlayer != null) {
-			Collider2D enemy = aiPlayer.findClosestEnemy(10);
-			if (enemy != null)
-				FireBullet((enemy.transform.position - gameObject.transform.position).normalized*4f, Vector3.zero);
+			Collider2D enemy = aiPlayer.findClosestEnemy (10);
+			if (enemy != null) {
+				Vector3 vec = (enemy.transform.position -
+				               gameObject.transform.position);
+				float angle = Mathf.Rad2Deg * Mathf.Atan2(vec.y, vec.x);
+				angle += Random.Range(-50f, 50f);
+				FireBullet (angle, 5f);
+			}
 		}
 	}
 }
